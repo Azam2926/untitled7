2cd52c19b32775750e885e455b3312368cf39bbb
macpro <ssd@mail.uz>
2023-03-31 16:56:15 +0500

    install vueuse

--- src/layouts/SystemBar.vue
+++ src/layouts/SystemBar.vue
@@ -1,10 +1,12 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
+import { useNow, useDateFormat } from '@vueuse/core'
 
+const formatted = useDateFormat(useNow(), 'HH:mm:ss a')
 const time = ref(new Date().toLocaleTimeString())
 setInterval(() => {
   time.value = new Date().toLocaleTimeString()
-})
+}, 100)
 </script>
 <template>
   <v-system-bar color="grey-darken-3">
@@ -12,6 +14,6 @@ setInterval(() => {
     <v-icon class="ms-2" icon="signal_cellular_alt"></v-icon>
     <v-icon class="ms-2" icon="battery_full"></v-icon>
 
-    <span class="ms-2">{{ time }}</span>
+    <span class="ms-2">{{ formatted }}</span>
   </v-system-bar>
 </template>
