028a11a8a4d3e568f884afb1e41a79aa21a3d6e5
macpro <ssd@mail.uz>
2023-03-31 11:55:39 +0500

    fix index error

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,11 @@
 <script lang="ts" setup>
 import { NDatePicker } from 'naive-ui'
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 
 const timestamp = ref<number[]>([0, 0])
+
+const timeBegin = computed(() => (timestamp.value?.length > 0 ? timestamp.value[0] : 0))
+const timeEnd = computed(() => (timestamp.value?.length > 1 ? timestamp.value[1] : 0))
 
 function handleChange(timestamps: number[]) {
   console.log(timestamps)
@@ -13,16 +16,13 @@ function handleChange(timestamps: number[]) {
   <div class="flex flex-col gap-4 items-center">
     <div class="rounded-full w-32 h-32 bg-green-500/75"></div>
     <div class="rounded-full w-32 h-32 bg-green-500"></div>
+
     <h1 class="text-3xl text-blue-500 text-center bg-amber-200 font-bold underline">
-      {{ timestamp }}
+      uz: {{ new Date(timeBegin).toLocaleString('uz') }} | {{ timeBegin }}
     </h1>
 
     <h1 class="text-3xl text-blue-500 text-center bg-amber-200 font-bold underline">
-      uz: {{ new Date(timestamp[0]).toLocaleString('uz') }}
-    </h1>
-
-    <h1 class="text-3xl text-blue-500 text-center bg-amber-200 font-bold underline">
-      uz: {{ new Date(timestamp[1]).toLocaleString('uz') }}
+      uz: {{ new Date(timeEnd).toLocaleString('uz') }} | {{ timeEnd }}
     </h1>
 
     <n-date-picker
