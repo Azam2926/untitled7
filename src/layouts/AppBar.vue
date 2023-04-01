5563e13f9d1337c4fd3d2f6ee36fbb785ed3a1ce
Azam <azam.inoyatov.1998@gmail.com>
2023-04-01 17:30:41 +0500

    remove app bar image

--- src/layouts/AppBar.vue
+++ src/layouts/AppBar.vue
@@ -4,15 +4,12 @@ import { useLayoutStore } from '@/stores/layout'
 const { toggleDrawer } = useLayoutStore()
 </script>
 <template>
-  <v-app-bar color="teal-darken-4" image="https://picsum.photos/1920/1080?random">
+  <v-app-bar color="teal-darken-4">
     <v-app-bar-title>
       <router-link to="/">Application bar</router-link>
     </v-app-bar-title>
     <template v-slot:prepend>
       <v-app-bar-nav-icon icon="menu" @click.stop="toggleDrawer" />
-    </template>
-    <template v-slot:image>
-      <v-img gradient="to top right, rgba(19,84,122,.8), rgba(128,208,199,.8)"></v-img>
     </template>
     <template v-slot:append>
       <v-btn icon="favorite"></v-btn>
