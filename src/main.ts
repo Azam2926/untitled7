ff2c48484c68a60e91b310ae46422b50617c119b
macpro <ssd@mail.uz>
2023-08-15 12:42:23 +0500

    add appwrite.io

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
 
 import '@/assets/main.css'
 import vuetify from '@/plugin/vuetify'
+import { Client } from 'appwrite'
+const client = new Client()
+
+client.setEndpoint('https://cloud.appwrite.io/v1').setProject('64db2ae2479811cd6b7c')
 
 const app = createApp(App)
 
